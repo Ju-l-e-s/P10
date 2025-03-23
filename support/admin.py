58753dde8e516a7b1d202416d28bd47a0f7febd4
Jules <jules.laconfourque@gmail.com>
2025-03-23 12:12:22 +0100

    Update admin.py

--- support/admin.py
+++ support/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User, Project, Contributor, Issue, Comment
 
-# add the custom User model to the Django admin
+# custom User model to the Django admin
 admin.site.register(User, UserAdmin)
 
-# add other models to manage via the admin
+# models to manage via the admin
 admin.site.register(Project)
 admin.site.register(Contributor)
 admin.site.register(Issue)
